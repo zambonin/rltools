728338240a92c8825640474611043f7fb016b342
Matheus Ben-Hur de Melo Leite <matheusbmleite@gmail.com>
2015-09-23 19:51:39 -0300

    debugging create_transitions

--- notamotua.py
+++ notamotua.py
@@ -12,53 +12,71 @@ class Automaton:
         self.new_transitions = {}
 
     def determinization(self):
-        determinizer(self.epsilon_closure())
+        self.determinizer(self.epsilon_closure())
     
     def epsilon_closure(self):
         _epsilon_closure = {}
         for state in self.states:
             if self.epsilon in self.transitions[frozenset([state])]: 
-                _epsilon_closure[state] = state | self.transitions[frozenset([state])]["λ"]
+                _epsilon_closure[state] = {state,} | self.transitions[frozenset([state])]["λ"]
             else:
                 _epsilon_closure[state] = state
         return _epsilon_closure
-    
-    #The code below is wrong!!!
-    def simple_determinizer(self):
-        opened = {(self.init_state,)} #I'm creating a state and forcing a tuple into it
-        closed = set()
+   
+    def determinizer(self, epsilon_closure):
+        opened = set(frozenset([epsilon_closure[self.init_state]]))
+        closed = set(frozenset())
         while opened:
             state = opened.pop()
-            closed.add(tuple(state))
-            for transition in self.transitions:
-                print("transition : ", transition)
-                if transition[0] == state[0]:   #If this statement is true, the loop breaks
-                    break
-                else:                       
-                    print("Creating transitions", state)
-                    self.create_transitions(state)
-                    #self.transitions = self.transitions and self.new_transitions
-                for transition in self.transitions:
-                    if transition[0] == state[0]:
-                        if tuple(self.transitions[transition]) not in opened | closed:
-                            opened.add(tuple(self.transitions[transition]))
-            print("Closed : ", closed)
-            print("Opened : ", opened)
-    
-    def epsilon_determinizer(self):
-        pass
+            if type(state) is frozenset: #added to don't insert frozenset of frozenset
+                closed.add(state)
+            else:
+                closed.add(frozenset([state]))
+            if frozenset([state]) in self.transitions.keys():
+                for key in self.transitions[frozenset([state])]:
+                    aux_state = self.transitions[frozenset([state])][key]
+                    new_state = set()
+                    for atom in aux_state: #atom is each part of the aux state (if the state is an union of two or more states)
+                        new_state.add(epsilon_closure[atom])
+                    if new_state not in opened | closed:
+                        opened.add(frozenset(new_state))
+            else:
+                self.create_transitions(state, epsilon_closure)
+                
 
-    def create_transitions(self, state):
+    def create_transitions(self, state, epsilon_closure):
         #TODO, create the transitions for the new state based on the union of the transitions from the states that
         #compose this one, remember to create a new dict of transitions to put this new ones and don't mix them with the 
         #transitions from the non-deterministic automaton
-        self.new_states.add(state)
-        for letter in self.alphabet:
-            auxiliar_set = set()
-            for element in state:
-                auxiliar_set = auxiliar_set | self.transitions[(element, letter)] 
-            self.new_transitions[(state,letter)] = auxiliar_set
-        print("New transitions : ", self.new_transitions) 
+        self.transitions[state] = {}
+        self.new_transitions[state] = {}
+        aux_set = ()
+        print("state : ", state)
+        for atom in state: #atom is each part of the new state
+            print("atom : ", atom)
+            for letter in self.alphabet: #for each letter of the alphabet
+                aux = self.transitions[frozenset([atom])][letter] #getting the transitions from atom by letter
+                print("auxiliar : ", aux, "by letter : ", letter)
+                if len(aux) > 1: #if the atom by the letter transits to more than one state
+                    print("len is greater : ", len(aux))
+                    for atom in aux: #getting the states that the atom transits by the letter to
+                        aux_set.add(epsilon_closure[atom]) #adding the epsilon closure from that arrival state to the aux_set
+                        print("aux set after addinf atom of atom : ", aux_set)
+                else:
+                    if aux: #if the atom by the letter transits to just one state
+                        aux_set.add(epsilon_closure[aux.pop()]) #popping that state from the set aux and adding their epsilon-closure to the aux_set
+                self.transitions[state][letter] = aux_set
+                print("trazii : ", self.transitions[state])
+                self.new_transitions[state][letter] = aux_set
+                print("aux set : ", aux_set)
+            aux_set.clear() #cleaning the set for the next iteration with another letter
+        print("transitions : ", self.transitions[state])
+
+
+
+
+
+
 def test():
     a = Automaton()
     a.states.add("q0")
@@ -84,11 +102,10 @@ def test():
     
     a.transitions[frozenset(["q3"])] = {}
     a.transitions[frozenset(["q3"])]["a"] = {"q3"}
-    a.transitions[frozenset(["q1"])]["b"] = {"q3"}
+    a.transitions[frozenset(["q3"])]["b"] = {"q3"}
     
 
     a.alphabet.add("a")
     a.alphabet.add("b")
-    print(a.epsilon_closure())
-    #a.determinization()
+    a.determinization()
 test()
