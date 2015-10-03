31e8e33335ad3be2d4dc71ae85a7914fea7aa97e
Gustavo Zambonin <gzmbnn@gmail.com>
2015-10-02 22:00:40 -0300

    missed linter on autotester

--- tests/autotester.sh
+++ tests/autotester.sh
@@ -8,20 +8,21 @@
 
 if [[ $1 ]]; then
     IFS='.' read -r filename ext <<< "$1"
-    ../rltools.py --dfa $filename.$ext
-    ../rltools.py --atg afd-$filename.out
+    ../rltools.py --dfa "$filename"."$ext"
+    ../rltools.py --atg "afd-$filename.out"
 
-    if [[ ! $(echo "$filename" | grep reg) ]]; then
-        ../rltools.py --gta gr-afd-$filename.out
-        ../rltools.py --atg afd-gr-afd-$filename.out
+    echo "$filename" | grep -q reg
+    if [ $? -ne 0 ] ; then
+        ../rltools.py --gta "gr-afd-$filename.out"
+        ../rltools.py --atg "afd-gr-afd-$filename.out"
 
-        if [ $(wc -l < afd-$filename.out) -eq \
-             $(wc -l < afd-gr-afd-$filename.out) ]; then
+        if [ "$(wc -l < "afd-$filename.out")" -eq \
+             "$(wc -l < "afd-gr-afd-$filename.out")" ]; then
             echo "automata pair is equal!"
         fi
 
-        if [ $(wc -l < gr-afd-$filename.out) -eq \
-             $(wc -l < gr-afd-gr-afd-$filename.out) ]; then
+        if [ "$(wc -l < "gr-afd-$filename.out")" -eq \
+             "$(wc -l < "gr-afd-gr-afd-$filename.out")" ]; then
             echo "grammar pair is equal!"
         fi
     fi
