2a7678fb2f58d64cd76187c495c9c15db2473509
MatheusBen-Hur <mtbmatheus@hotmail.com>
2015-11-08 19:04:39 -0200

    string recognizer added

--- algorithms/complex_builder.py
+++ algorithms/complex_builder.py
@@ -10,7 +10,9 @@ class complex_builder():
     numbers = '0|1|2|3|4|5|6|7|8|9'
     nonzero_num = '1|2|3|4|5|6|7|8|9'
     underscore = '_'
-    #closure_identifier = "{}{}{}{}".format(simple_identifier, numbers, underscore, '*')
+    quote = '"'
+    string = " |!|#|$|%|&|'|+|,|-|.|/|0|1|2|3|4|5|6|7|8|9|:|;|<|=|>|?|@|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|[|]|^|_|`|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|{|}|~"
+
 
     list_regs = list()
     list_regs.append(simple_identifier)
@@ -88,16 +90,39 @@ class complex_builder():
 
     finals_aut.append(aux_aut2)
 
+
+    #STARTING THE STRING AUTOMATON--------------------------------------|
+    list_regs = list()
+    list_regs.append(quote)
+    list_regs.append(string)
+    list_regs.append(quote)
+
+    list_auts = list()
+
+    for reg in list_regs:
+        reg_exp = RegularExpression(reg)
+        aut = reg_exp.regexp_to_automaton()
+        aut.minimize()
+        list_auts.append(aut)
+    reg_aux = RegularExpression("")
+
+    automatons = list()
+    automatons.append(list_auts.pop(1))
+    aux_aut = reg_aux.closure_op(automatons)
+    list_auts.insert(1, aux_aut)
+
+    automatons = list()
+    automatons.append(list_auts.pop(0))
+    automatons.append(list_auts.pop(0))
+    aux_aut = reg_aux.concat_op(automatons)
+    list_auts.insert(0, aux_aut)
+
+    aux_aut = reg_aux.concat_op(list_auts)
+    aux_aut2 = reg_aux.rename_aut(aux_aut)
+    aux_aut2.minimize()
+
+    finals_aut.append(aux_aut2)
+
 def test():
    c = complex_builder()
 test()
-
-"""
-# digit ::= '0' | '1' | ... | '9'
-    # nonzerodigit ::= '1' | ... | '9'
-    # integer ::= nonzerodigit digit* | '0'"""
-
-
-"""
-int: (((1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*)|0)
-str: ("( |!|#|$|%|&|'|+|,|-|.|/|0|1|2|3|4|5|6|7|8|9|:|;|<|=|>|?|@|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|[|]|^|_|`|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|{|}|~)*")¨ """
